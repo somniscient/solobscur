8a8ed88b5badde92dfc0e317bbb17061287c182e
somniscient <syrdanikthegrim@icloud.com>
2026-03-18 16:08:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ gsap.register.Plugin(ScrollTrigger);
 
 function draw() {
   const ctx = document.getElementById("canvas").getContext("2d");
-  ctx.font = "48px sans serif";
+  ctx.font = "48px";
   ctx.fillText("SK", 10, 50);
 }
