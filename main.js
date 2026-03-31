004c8854c1095be56f3d11584cf3351a8fa2fe3e
somniscient <syrdanikthegrim@icloud.com>
2026-03-31 13:30:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 function draw() {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  ctx.font = "3.0em";
+  ctx.font = "60px sans-serif";
   ctx.fillText("SK", 10, 50);
 }
 draw();
